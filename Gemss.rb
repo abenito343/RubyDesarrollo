6893532342c2bed3d4b18c033f10bff6df103b8a
Ariel <abenito@frba.utn.edu.ar>
2024-02-29 02:55:53 -0300

    backup

---
+++ Gemss.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'dni_nie'
+
+puts DniNie.letra(45050505)
+puts DniNie.validar_doc '45050505Z'
+puts DniNie.validar_doc '45050505X'
+
+## asdasdasdasdasdasdasdasdsadsa
