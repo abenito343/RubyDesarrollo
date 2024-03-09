2920628ce795431aa28bf0ecb3aef1be9cfde37f
Ariel <abenito@frba.utn.edu.ar>
2024-03-09 18:23:27 -0300

    automatizacionDDS.rb

--- automatizacionDDS.rb
+++ automatizacionDDS.rb
@@ -8,9 +8,73 @@ sleep 1
 
 #ir a tal URL
 driver.get "http://localhost:8080/login"
+sleep 1
+
+
+##Probar boton register de login
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab , :tab , :tab , :tab , :tab).perform
+
+sleep 1
+# Presiona "Enter"
+driver.action.send_keys(:enter).perform
+
+
+
+##Completar datos de Register
+campo1 = driver.find_element(:id, 'firstName')
+campo1.send_keys('Lucas')
+sleep 1
+
+campo2 = driver.find_element(:id, 'lastName')
+campo2.send_keys('Lopez')
+sleep 1
+
+campo3 = driver.find_element(:id, 'email')
+campo3.send_keys('lucaslopez@gmail.com')
+sleep 1
+
+campo4 = driver.find_element(:id, 'numero')
+campo4.send_keys('1515151515')
+sleep 1
+
+campo5 = driver.find_element(:id, 'password')
+campo5.send_keys('Lucaslopez123@@@')
+sleep 1
+
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab).perform
+
+sleep 1
+# Presiona "Enter"
+driver.action.send_keys(:enter).perform
+
+
+sleep 2
+
+
+
+
+###Ir a Index
+# Envía "Tab"
+driver.action.send_keys(:tab).perform
+sleep 1
+
+# Presiona "Enter"
+driver.action.send_keys(:enter).perform
+sleep 3
+
+
+###Ir a Login
+driver.action.send_keys(:tab, :tab, :tab).perform
+sleep 1
+driver.action.send_keys(:enter).perform
+sleep 1
+
 
 sleep 1
 
+###Loguearse
 # Encuentra el primer campo de texto por su identificador, nombre, XPath, u otro selector
 primer_campo = driver.find_element(:id, 'username')
 primer_campo.send_keys('lucaslopez@gmail.com')
@@ -21,10 +85,12 @@ segundo_campo.send_keys('Lucaslopez123@@@', :enter)
 
 sleep 2
 
-# Encuentra el botón por su identificador, nombre, XPath, u otro selector        NO PUDE
-#boton = driver.find_element(:id, 'navbarSupportedContent')
-#boton.click
 
+
+
+
+
+### Entrar en users
 sleep 1
 # Envía dos teclas "Tab"
 driver.action.send_keys(:tab, :tab).perform
@@ -35,6 +101,68 @@ driver.action.send_keys(:enter).perform
 
 
 
-sleep 5
+### Entrar en Profile
+sleep 1
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab, :tab).perform
+
+sleep 1
+# Presiona "Enter"
+driver.action.send_keys(:enter).perform
+
+
+
+### Entrar en communities
+sleep 1
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab).perform
+
+sleep 1
+driver.action.send_keys(:enter).perform
+
+
+### Entrar en ADD Incident
+sleep 1
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab, :tab, :tab, :tab).perform
+
+sleep 1
+# Presiona "Enter"
+driver.action.send_keys(:enter).perform
+
+
+##Completar datos de Register
+campo11 = driver.find_element(:id, 'observation')
+campo11.send_keys('se rompio el baño upsi')
+sleep 1
+
+campo12 = driver.find_element(:id, 'Service')
+campo12.send_keys('1')
+sleep 1
+
+campo13 = driver.find_element(:id, 'tipo_incidente')
+campo13.send_keys('1')
+sleep 1
+
+
+### Entrar en Lista de incidentes
+sleep 1
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab).perform
+
+sleep 1
+driver.action.send_keys(:enter).perform
+
+
+### Desloguearse
+sleep 1
+# Envía dos teclas "Tab"
+driver.action.send_keys(:tab, :tab, :tab, :tab, :tab).perform
+
+sleep 1
+driver.action.send_keys(:enter).perform
+
+
+sleep 1
 #cerrar pagina
 driver.quit
